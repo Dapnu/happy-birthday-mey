308ebaa8ae486332b8833c40a3eea8c10cd5979d
Daffa Khusnureza <111774079+Dapnu@users.noreply.github.com>
2025-04-01 03:06:33 +0700

    Update main.js

--- script/main.js
+++ script/main.js
@@ -298,7 +298,7 @@ const animationTimeline = () => {
   // Restart Animation on click
   const replyBtn = document.getElementById("replay");
   replyBtn.addEventListener("click", () => {
-    tl.restart();
+    window.open("https://iloveyoumeymwah.carrd.co/#", "_blank");
   });
 };
 
